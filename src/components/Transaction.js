912af2e5b584115ea6ff95ba40f4fd2b601bbd15
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-28 22:00:14 +0300

    transaction render

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,14 @@
 import React from "react";
-
-function Transaction() {
-  return (
-    <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-    </tr>
-  );
+//receives the props needed for what is supposed to show on the screen from Transaction
+function Transaction({date, description, category,amount}) {
+      return (
+        <tr>
+          <td>{date}</td>
+          <td>{description}</td>
+          <td>{category}</td>
+          <td>{amount}</td>
+        </tr>
+      );
 }
 
 export default Transaction;
