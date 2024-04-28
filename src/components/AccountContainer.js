7ab10d1f42ed4325bb23f4e912d1f9f4602dc95c
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-28 21:55:25 +0300

    intial commit for the fetch GET

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactionArray, setTransaction] = useState([])
+  const [searchText, setSearchFilter] = useState("")
+  const url = "http://localhost:8001/transactions"
+
+/*fetch transaction array from the server and useState to store for us */
+fetch(url)
+  .then(resp=>resp.json())
+    .then(data=>setTransaction(data))
+
+function handleNewTransaction(newInputs){
+  setTransaction(transactions=>[...transactions,newInputs])
+
+//persist new data to server from the form
+fetch(url,{
+  method: 'POST',
+  headers: {
+    "Content-Type" : "application/json"
+  },
+  body: JSON.stringify(newInputs)
+  })
+}
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      
+      <Search searchText={searchText} 
+        onFilterTextChange={setSearchFilter} />
+      <AddTransactionForm 
+      onFormSubmit = {handleNewTransaction}
+      />
+      {/* requires two things as props, transactions and search word(s) in state */}
+      <TransactionsList transactions = {transactionArray}
+      searchText = {searchText}
+      /> 
     </div>
   );
 }
