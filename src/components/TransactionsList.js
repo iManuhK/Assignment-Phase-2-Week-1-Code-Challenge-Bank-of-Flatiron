5a78be0e0daf22cffcfd86e85d9efb215d9aeece
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-28 22:02:17 +0300

    sets up transaction list render component

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
+//pass in the array of transactions (existing and new) and the search keyword(s) in state
+function TransactionsList({transactions, searchText}) {
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -20,6 +21,28 @@ function TransactionsList() {
           </th>
         </tr>
         {/* render a list of <Transaction> components here */}
+        {/* use the typed in searchword here as this is the Component that renders the transactions
+        and we have both new and old transactions from the server */}
+
+        {/* We also have a .map() method that allows us to conditionally render
+        our DOM elements inside of JSX */}
+        {transactions.map(transaction => {
+          if (
+            transaction.description.toLowerCase().indexOf(
+              searchText.toLowerCase()
+            ) === -1
+          ){return}
+          else {
+          return (<Transaction 
+                  key = {transaction.id} 
+                  date = {transaction.date} 
+                  description = {transaction.description} 
+                  category = {transaction.category}
+                  amount = {transaction.amount}
+                  />)
+            }
+                })
+        } 
       </tbody>
     </table>
   );
